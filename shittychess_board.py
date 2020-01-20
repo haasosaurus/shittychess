47566d8bf8161f231ef6622eaad1ed38c77996be
Jim Haas <haasasaurus@gmail.com>
2020-01-20 12:57:50 -0800

    changed constructor and expanding methods for highlighting and bordering spaces

--- shittychess_board.py
+++ shittychess_board.py
@@ -3,21 +3,28 @@
 
 import pygame
 
-from shittychess_settings import ShittySettings
-from shittychess_logic import ShittyLogic
+from shittychess_pieces import ShittyPiece
 
 
 class ShittyBoard:
     """this class represents a chess board"""
 
-    def __init__(self, screen: pygame.Surface, settings: ShittySettings, logic: ShittyLogic) -> None:
-        self.screen = screen
-        self.settings = settings
-        self.logic = logic
-        self.tile_image = pygame.image.load(self.settings.tile_image_path)
-        self.tile_rect = self.tile_image.get_rect()
+    def __init__(self) -> None:
+        self.screen = None  # pygame.Surface
+        self.settings = None  # ShittySettings
+        self.logic = None  # ShittyLogic
+        self.tile_image = None  # pygame.image
+        self.tile_rect = None  # pygame.Rect
         self.col_header_labels = []
         self.row_header_labels = []
+
+    def configure(self) -> None:
+        """
+        configure class's properties after they have been assigned externally
+        """
+
+        self.tile_image = pygame.image.load(self.settings.tile_image_path)
+        self.tile_rect = self.tile_image.get_rect()
         self.render_header_labels()
 
     def resize_header_label_font(self, font_sz: int) -> None:
@@ -46,7 +53,7 @@ class ShittyBoard:
             self.settings.header_font_width = tmp_rect.width
             self.settings.header_font_height = tmp_rect.height
 
-    def draw(self) -> None:
+    def draw(self, debug_piece=None) -> None:
         """
         draws all board elements on the screen
         """
@@ -63,8 +70,8 @@ class ShittyBoard:
             self.draw_headers()
 
         # debug testing space highlight
-        if self.settings.debug:
-            self.highlight_space('a1')
+        if self.settings.debug and debug_piece:
+            self.highlight_valid_moves(debug_piece)
 
     def draw_headers(self) -> None:
         """
@@ -92,20 +99,19 @@ class ShittyBoard:
             tmp_rect.left = self.settings.row_header_x_right()
             self.screen.blit(label, tmp_rect)
 
-    def highlight_space(self, coords: str) -> None:
+
+    def draw_space_border(self, rect: pygame.Rect, color=(255, 255, 255), alpha=255) -> None:
         """
-        highlights and borders a space on the board
-        should be used to show available moves to a player
+        borders a space based on rect argument
         """
 
         side_rect_width = 2
         side_rect_height = int(self.settings.space_height() / 3)
         top_bottom_rect_width = int(self.settings.space_width() / 3)
         top_bottom_rect_height = 2
-        tmp_rect = pygame.Rect.copy(self.logic.coords_to_rect(coords))
 
         # calculate rects for the sides
-        right_top = pygame.Rect(tmp_rect.left, tmp_rect.top, side_rect_width, side_rect_height)
+        right_top = pygame.Rect(rect.left, rect.top, side_rect_width, side_rect_height)
         right_bottom = pygame.Rect.copy(right_top)
         right_bottom.top += self.settings.space_height() - side_rect_height
         left_top = pygame.Rect.copy(right_top)
@@ -114,7 +120,7 @@ class ShittyBoard:
         left_bottom.left += self.settings.space_width() - side_rect_width
 
         # calculate rects for the top and bottom
-        top_left = pygame.Rect(tmp_rect.left, tmp_rect.top, top_bottom_rect_width, top_bottom_rect_height)
+        top_left = pygame.Rect(rect.left, rect.top, top_bottom_rect_width, top_bottom_rect_height)
         bottom_left = pygame.Rect.copy(top_left)
         bottom_left.top += self.settings.space_height() - top_bottom_rect_height
         top_right = pygame.Rect.copy(top_left)
@@ -123,17 +129,54 @@ class ShittyBoard:
         bottom_right.top += self.settings.space_height() - top_bottom_rect_height
 
         # draw the lines around the target square
-        pygame.draw.rect(self.screen, self.settings.color_white, right_top)
-        pygame.draw.rect(self.screen, self.settings.color_white, right_bottom)
-        pygame.draw.rect(self.screen, self.settings.color_white, left_top)
-        pygame.draw.rect(self.screen, self.settings.color_white, left_bottom)
-        pygame.draw.rect(self.screen, self.settings.color_white, top_left)
-        pygame.draw.rect(self.screen, self.settings.color_white, bottom_left)
-        pygame.draw.rect(self.screen, self.settings.color_white, top_right)
-        pygame.draw.rect(self.screen, self.settings.color_white, bottom_right)
+        tmp_color = pygame.Color(*color)
+        tmp_color.a = alpha
+        pygame.draw.rect(self.screen, tmp_color, right_bottom)
+        pygame.draw.rect(self.screen, tmp_color, right_top)
+        pygame.draw.rect(self.screen, tmp_color, left_top)
+        pygame.draw.rect(self.screen, tmp_color, left_bottom)
+        pygame.draw.rect(self.screen, tmp_color, top_left)
+        pygame.draw.rect(self.screen, tmp_color, bottom_left)
+        pygame.draw.rect(self.screen, tmp_color, top_right)
+        pygame.draw.rect(self.screen, tmp_color, bottom_right)
 
-        # highlight the background of the target square
-        tmp_color = pygame.Color(255, 255, 255, 100)
+    def draw_space_highlight(self, rect: pygame.Rect, color=(255, 255, 255), alpha=100) -> None:
+        """
+        highlights a space based on rect argument
+        """
+
+        tmp_color = pygame.Color(*color)
+        tmp_color.a = alpha
         tmp_surface = pygame.Surface((self.settings.space_width(), self.settings.space_height())).convert_alpha()
         tmp_surface.fill(tmp_color)
-        self.screen.blit(tmp_surface, tmp_rect)
+        self.screen.blit(tmp_surface, rect)
+
+    def highlight_space(self, coords: str, space_color=(255, 255, 255), space_alpha=100) -> None:
+        """
+        takes chess coordinates and gets a rect for it, then calls self.draw_space_highlight on that rect
+        """
+
+        tmp_rect = pygame.Rect.copy(self.logic.coords_to_rect(coords))
+        self.draw_space_highlight(tmp_rect, color=space_color, alpha=space_alpha)
+
+    def highlight_and_border_space(self, coords: str, space_color=(255, 255, 255), space_alpha=100, border_color=(255, 255, 255), border_alpha=255) -> None:
+        """
+        highlights and borders a space on the board from a chess coord str
+        should be used to show available moves to a player
+        """
+
+        tmp_rect = pygame.Rect.copy(self.logic.coords_to_rect(coords))
+        self.draw_space_highlight(tmp_rect, color=space_color, alpha=space_alpha)
+        self.draw_space_border(tmp_rect, color=border_color, alpha=border_alpha)
+
+    def highlight_valid_moves(self, piece: ShittyPiece) -> None:
+        """
+        takes a reference to a ShittyPiece and highlights that piece,
+        then it highlights and borders all valid moves for that piece
+        """
+
+        if piece:
+            self.highlight_space(piece.coords, space_alpha=150)
+            valid_spaces = self.logic.valid_moves(piece)
+            for space in valid_spaces:
+                self.highlight_and_border_space(space)
