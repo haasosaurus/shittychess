fa260dc32249956e2f716c06d979b0ba1795e159
Jim Haas <haasasaurus@gmail.com>
2020-01-21 00:46:24 -0800

    implemented moving pieces with mouse, a resize method, improved type hinting

--- shittychess_events.py
+++ shittychess_events.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 
 
-import sys
+from typing import NoReturn
+
 import pygame
 
 
@@ -12,30 +13,154 @@ class ShittyEventMonitor:
     it does more than monitor
     """
 
-    def __init__(self) -> None:
+    def __init__(self) -> NoReturn:
         self.screen = None  # pygame.Surface
         self.settings = None  # ShittySettings
         self.layout = None  # ShittyLayout
+        self.logic = None  # ShittyLogic
+        self.board = None  # ShittyBoard
 
-    # def configure(self) -> None:
+        # initialize mouse press bools
+        self.left_mouse_pressed = False
+        self.middle_mouse_pressed = False
+        self.right_mouse_pressed = False
+
+        # save last frame's mouse position
+        self.left_mouse_last_frame = False
+        self.middle_mouse_last_frame = False
+        self.right_mouse_last_frame = False
+
+        # initialize mouse click bools
+        self.left_mouse_click = False
+        self.middle_mouse_click = False
+        self.right_mouse_click = False
+
+        # piece moving bools
+        self.holding_piece = False
+        self.holding_piece_sprite = None
+        self.holding_piece_original_rect = None
+
+    # def configure(self) -> NoReturn:
     #     """
     #     configure class's properties after they have been assigned externally
     #     """
 
     #     pass
 
-    def process_events(self) -> None:
+    def process_events(self) -> NoReturn:
         """
         process pygame events
         """
 
+        # complete mouse clicks with no looping
+        self.left_mouse_click = False
+        self.middle_mouse_click = False
+        self.right_mouse_click = False
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
+
+                # exit game
                 if event.key == pygame.K_q:
-                    sys.exit()
+                    self.settings.exiting = True
+
+                # toggle headers enabled
                 if event.key == pygame.K_h:
                     self.settings.headers_enabled = not self.settings.headers_enabled
                     self.screen = pygame.display.set_mode((self.settings.screen_width(), self.settings.screen_height()))
                     self.layout.resize()
+                    self.logic.resize()
+
+                # toggle player turn
+                if event.key == pygame.K_t:
+                    self.settings.turn_black = not self.settings.turn_black
+
+            # exit game
             elif event.type == pygame.QUIT:
-                sys.exit()
+                self.settings.exiting = True
+
+            # press mouse buttons
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                # left mouse button
+                if event.button == 1:
+                    self.left_mouse_pressed = True
+                    if not self.left_mouse_last_frame:
+                        self.left_mouse_click = True
+
+                    # piece movement - picking up
+                    if not self.holding_piece:
+                        self.board.sprite_to_highlight = None
+                        x, y = pygame.mouse.get_pos()
+                        _, _ = pygame.mouse.get_rel()
+                        if self.layout.sprite_clicked(x, y, black=self.settings.turn_black):
+                            sprite = self.layout.click_to_sprite(x, y, black=self.settings.turn_black)
+                            self.board.sprite_to_highlight = sprite
+                            self.holding_piece_original_rect = pygame.Rect.copy(sprite.rect)
+                            self.holding_piece_sprite = sprite
+                            self.holding_piece = True
+
+                # middle mouse button
+                elif event.button == 2:
+                    self.middle_mouse_pressed = True
+                    if not self.middle_mouse_last_frame:
+                        self.middle_mouse_click = True
+
+                    # highlight available moves
+                    if not self.holding_piece:
+                        x, y = pygame.mouse.get_pos()
+                        if self.layout.sprite_clicked(x, y, black=self.settings.turn_black):
+                            sprite = self.layout.click_to_sprite(x, y, black=self.settings.turn_black)
+                            if sprite:
+                                self.board.sprite_to_highlight = sprite
+                            else:
+                                self.board.sprite_to_highlight = None
+                        else:
+                            self.board.sprite_to_highlight = None
+
+                # right mouse button
+                elif event.button == 3:
+                    self.right_mouse_pressed = True
+                    if not self.right_mouse_last_frame:
+                        self.right_mouse_click = True
+
+            # release mouse buttons
+            elif event.type == pygame.MOUSEBUTTONUP:
+
+                # left mouse button
+                if event.button == 1:
+                    self.left_mouse_pressed = False
+
+                    # piece movement - dropping
+                    if self.holding_piece:
+                        x, y = pygame.mouse.get_pos()
+                        if not self.logic.move_piece_xy(self.holding_piece_sprite, x, y):
+                            self.holding_piece_sprite.rect = self.holding_piece_original_rect
+                        self.board.sprite_to_highlight = None
+                        self.holding_piece = False
+                        self.holding_piece_sprite = None
+                        self.holding_piece_original_rect = None
+
+                # middle mouse button
+                elif event.button == 2:
+                    self.middle_mouse_pressed = False
+
+                # right mouse button
+                elif event.button == 3:
+                    self.right_mouse_pressed = False
+
+            # mouse motion
+            elif event.type == pygame.MOUSEMOTION:
+
+                # piece movement - motion
+                if self.holding_piece:
+                    x, y = pygame.mouse.get_rel()
+                    self.holding_piece_sprite.rect.left += x
+                    self.holding_piece_sprite.rect.top += y
+
+
+
+        # set mouse button last frame to current one
+        self.left_mouse_last_frame = self.left_mouse_pressed
+        self.middle_mouse_last_frame = self.middle_mouse_pressed
+        self.right_mouse_last_frame = self.right_mouse_pressed
--- shittychess_logic.py
+++ shittychess_logic.py
@@ -3,16 +3,29 @@
 
 from typing import List
 from typing import Tuple
+from typing import Union
+from typing import NoReturn
 
 import pygame
 
+from shittychess_utils import ShittyMousePointer
 from shittychess_pieces import ShittyPiece
 
 
-class ShittyLogic:
-    """game logic class"""
+class ShittySpace(pygame.sprite.Sprite):
 
-    def __init__(self) -> None:
+    def __init__(self, rect: pygame.Rect, coords: str) -> NoReturn:
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = rect
+        self.coords = coords
+
+class ShittyLogic:
+    """
+    game 'logic' class
+    the shittiest logic
+    """
+
+    def __init__(self) -> NoReturn:
         self.settings = None  # ShittySettings
         self.board = None  # ShittyBoard
         self.layout = None # ShittyLayout
@@ -20,8 +33,10 @@ class ShittyLogic:
         self.__rect_to_coords = {}
         self.__coords_to_indexes = {}
         self.__indexes_to_coords = {}
+        self.__spaces_to_coords = {}
+        self.__spaces_group = pygame.sprite.Group()
 
-    def configure(self) -> None:
+    def configure(self) -> NoReturn:
         """
         configure class's properties after they have been assigned externally
         this sets up all the dicts and their matching dicts that i thought we needed to
@@ -29,8 +44,8 @@ class ShittyLogic:
         """
 
         self.settings.coords_list = []
-        for row, y in zip(self.settings.row_headers, range(0, int(self.settings.space_height() * self.settings.rows), self.settings.space_height())):
-            for col, x in zip(self.settings.col_headers, range(0, int(self.settings.space_width() * self.settings.cols), self.settings.space_width())):
+        for row, y in zip(self.settings.row_headers, range(0, int(self.settings.board_height()), self.settings.space_height())):
+            for col, x in zip(self.settings.col_headers, range(0, int(self.settings.board_width()), self.settings.space_width())):
                 pos_name = col + row
                 tmp_rect = pygame.Rect(x, y, self.settings.space_width(), self.settings.space_height())
                 self.__coords_to_rect.update({pos_name: tmp_rect})
@@ -42,57 +57,75 @@ class ShittyLogic:
             self.__indexes_to_coords.update({indexes: coords})
         for coords, rect in self.__coords_to_rect.items():
             self.__rect_to_coords.update({(rect.left, rect.top, rect.width, rect.height): coords})
+            tmp_rect = pygame.Rect.copy(rect)
+            tmp_rect.left += self.settings.board_start_x()
+            tmp_rect.top += self.settings.board_start_y()
+            self.__spaces_to_coords.update({ShittySpace(tmp_rect, coords): coords})
+        for space in self.__spaces_to_coords:
+            self.__spaces_group.add(space)
 
-    def coords_to_indexes(self, coords: str) -> Tuple[int]:
+    def resize(self) -> NoReturn:
+        for sprite, coords in self.__spaces_to_coords.items():
+            sprite.rect = self.coords_to_rect(coords)
+
+    def xy_to_coords(self, x: int, y: int) -> Union[str, None]:
+        collisions = pygame.sprite.spritecollide(ShittyMousePointer(x, y), self.__spaces_group, False)
+        if len(collisions) == 1:
+            if collisions[0] in self.__spaces_to_coords:
+                return self.__spaces_to_coords[collisions[0]]
+        return None
+
+    def coords_to_indexes(self, coords: str) -> Union[Tuple[int, int], None]:
         """
         takes chess coordinates
         returns the corresponding zero-indexed tuple with 2D list indexes for the board space,
+        or None if not found
         0, 5 = ShittyLogic.coords_to_indexes('f4')
         """
 
         if coords in self.__coords_to_indexes:
             return self.__coords_to_indexes[coords]
-        return ()
+        return None
 
-    def coords_to_rect(self, coords: str) -> pygame.Rect:
+    def coords_to_rect(self, coords: str) -> Union[pygame.Rect, None]:
         """
         takes chess coordinates
-        returns a pygame.Rect object of the corresponding board space,
+        returns a pygame.Rect object of the corresponding board space or None if not found
         pygame.Rect(0, 120, 60, 60) = ShittyLogic.coords_to_rect('f4')
         """
 
         if coords not in self.__coords_to_rect:
-            return pygame.Rect(0, 0, 0, 0)
+            return None
         tmp_rect = pygame.Rect(self.__coords_to_rect[coords])
         if self.settings.headers_enabled:
             tmp_rect.left += self.settings.row_header_width()
             tmp_rect.top += self.settings.col_header_height()
         return tmp_rect
 
-    def indexes_to_coords(self, indexes: Tuple[int]) -> str:
+    def indexes_to_coords(self, indexes: Tuple[int, int]) -> Union[str, None]:
         """
         takes a zero-indexed tuple with 2D list indexes
-        returns the corresponding chess notation for a board space,
+        returns the corresponding chess notation for a board space or None if not found
         'f4' = ShittyLogic.indexes_to_coords(0, 5)
         """
 
         if indexes in self.__indexes_to_coords:
             return self.__indexes_to_coords[indexes]
-        return ''
+        return None
 
-    def indexes_to_rect(self, indexes: Tuple[int]) -> pygame.Rect:
+    def indexes_to_rect(self, indexes: Tuple[int, int]) -> Union[pygame.Rect, None]:
         """
         takes a zero-indexed tuple with 2D list indexes
-        returns a pygame.Rect object of the corresponding board space,
+        returns a pygame.Rect object of the corresponding board space or None if not found
         pygame.Rect(0, 120, 60, 60) = ShittyLogic.indexes_to_rect(0, 5)
         """
 
         return self.coords_to_rect(self.indexes_to_coords(indexes))
 
-    def rect_to_coords(self, rect: pygame.Rect) -> str:
+    def rect_to_coords(self, rect: pygame.Rect) -> Union[str, None]:
         """
         takes a pygame.Rect object
-        returns the corresponding chess notation for a board space,
+        returns the corresponding chess notation for a board space or None if not found
         'f4' = ShittyLogic.rect_to_coords(pygame.Rect(0, 120, 60, 60))
         """
 
@@ -105,19 +138,20 @@ class ShittyLogic:
             top -= self.settings.col_header_height()
         tmp_tuple = (left, top, width, height)
         if tmp_tuple not in self.__rect_to_coords:
-            return ''
+            return None
         return self.__rect_to_coords[(left, top, width, height)]
 
-    def rect_to_indexes(self, rect: pygame.Rect) -> Tuple[int]:
+    def rect_to_indexes(self, rect: pygame.Rect) -> Union[Tuple[int, int], None]:
         """
         takes a pygame.Rect object
         returns the corresponding zero-indexed tuple with 2D list indexes for the board space,
+        or None if not found
         0, 5 = ShittyLogic.rect_to_indexes(pygame.Rect(0, 120, 60, 60))
         """
 
         return self.coords_to_indexes(self.rect_to_coords(rect))
 
-    def valid_space_coords(self, piece: ShittyPiece) -> List[str]:
+    def __valid_space_coords(self, piece: ShittyPiece) -> List[str]:
         """
         make a list of valid moves for a piece that are on the game board,
         no other error checking performed. return them as a list of chess
@@ -143,12 +177,12 @@ class ShittyLogic:
 
     def valid_move_coords(self, piece: ShittyPiece) -> List[str]:
         """
-        get valid move space list from self.valid_space_coords, and run more in
+        get valid move space list from self.__valid_space_coords, and run more in
         depth tests on them to make sure they are really valid move coordinates
         """
 
         # check to see if a friendly piece is blocking
-        valid_move_coords = self.valid_space_coords(piece)
+        valid_move_coords = self.__valid_space_coords(piece)
         piece_black = piece.black
         invalid_move_coords = []
         for coords in valid_move_coords:
@@ -177,3 +211,15 @@ class ShittyLogic:
 
         # return a list of chess coordinates strings
         return valid_move_coords
+
+    def move_piece_xy(self, sprite: pygame.sprite.Sprite, x, y) -> bool:
+        print('logic.move_piece_xy')
+        target_coords = self.xy_to_coords(x, y)
+        if target_coords:
+            if target_coords != sprite.coords:
+                valid_move_coords = self.valid_move_coords(sprite)
+                if target_coords in valid_move_coords:
+                    sprite.move(self.coords_to_rect(target_coords))
+                    sprite.coords = target_coords
+                    return True
+        return False
