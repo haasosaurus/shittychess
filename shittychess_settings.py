2d7bd191128100120b889fc3ac95fa9cfefb0ea4
Jim Haas <haasasaurus@gmail.com>
2020-01-22 04:26:28 -0800

    logic refactor and added another piece set option

--- shittychess_settings.py
+++ shittychess_settings.py
@@ -14,29 +14,51 @@ class ShittySettings:
         self.debug = True
         self.cols = 8
         self.rows = 8
-        self.tile_image_path_obj = pathlib.Path('shitty_art/shittychess_tile.png')
-        self.tile_image_path = str(self.tile_image_path_obj)
-        self.tile_w = 120
-        self.tile_h = 120
-        self.cols_per_tile = 2
-        self.rows_per_tile = 2
-        self.coords_list = []
+        self.__space_width = 60
+        self.__space_height = 60
+        self.space_path_wood_black = str(pathlib.Path('shitty_art/space_wood_black.png'))
+        self.space_path_wood_white = str(pathlib.Path('shitty_art/space_wood_white.png'))
+
+        # game piece paths
+        self.shitty_pawn_black_path = str(pathlib.Path('shitty_art/shittypawnblack.png'))
+        self.shitty_pawn_white_path = str(pathlib.Path('shitty_art/shittypawnwhite.png'))
+        self.shitty_rook_black_path = str(pathlib.Path('shitty_art/shittyrookblack.png'))
+        self.shitty_rook_white_path = str(pathlib.Path('shitty_art/shittyrookwhite.png'))
+        self.shitty_bishop_black_path = str(pathlib.Path('shitty_art/shittybishopblack.png'))
+        self.shitty_bishop_white_path = str(pathlib.Path('shitty_art/shittybishopwhite.png'))
+        self.shitty_knight_black_path = str(pathlib.Path('shitty_art/shittyknightblack.png'))
+        self.shitty_knight_white_path = str(pathlib.Path('shitty_art/shittyknightwhite.png'))
+        self.shitty_queen_black_path = str(pathlib.Path('shitty_art/shittyqueenblack.png'))
+        self.shitty_queen_white_path = str(pathlib.Path('shitty_art/shittyqueenwhite.png'))
+        self.shitty_king_black_path = str(pathlib.Path('shitty_art/shittykingblack.png'))
+        self.shitty_king_white_path = str(pathlib.Path('shitty_art/shittykingwhite.png'))
+
+        self.trad_pawn_black_path = str(pathlib.Path('shitty_art/trad_pawn_black.png'))
+        self.trad_pawn_white_path = str(pathlib.Path('shitty_art/trad_pawn_white.png'))
+        self.trad_rook_black_path = str(pathlib.Path('shitty_art/trad_rook_black.png'))
+        self.trad_rook_white_path = str(pathlib.Path('shitty_art/trad_rook_white.png'))
+        self.trad_bishop_black_path = str(pathlib.Path('shitty_art/trad_bishop_black.png'))
+        self.trad_bishop_white_path = str(pathlib.Path('shitty_art/trad_bishop_white.png'))
+        self.trad_knight_black_path = str(pathlib.Path('shitty_art/trad_knight_black.png'))
+        self.trad_knight_white_path = str(pathlib.Path('shitty_art/trad_knight_white.png'))
+        self.trad_queen_black_path = str(pathlib.Path('shitty_art/trad_queen_black.png'))
+        self.trad_queen_white_path = str(pathlib.Path('shitty_art/trad_queen_white.png'))
+        self.trad_king_black_path = str(pathlib.Path('shitty_art/trad_king_black.png'))
+        self.trad_king_white_path = str(pathlib.Path('shitty_art/trad_king_white.png'))
 
         # header variables declarations/initializations
         self.headers_enabled = True
-        self.__row_header_w = 30
-        self.__col_header_h = 30
+        self.__row_header_width = 30
+        self.__col_header_height = 30
         self.col_headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         self.row_headers = ['8', '7', '6', '5', '4', '3', '2', '1']
         self.header_background_color = (30, 30, 30)
         self.header_font_color = (221, 221, 221)
-        self.header_font_sz = 16
+        self.header_font_sz = 15
         self.header_font_width = self.header_font_sz
         self.header_font_height = self.header_font_sz
-        self.header_font_path_obj = pathlib.Path('fonts/LiberationMono-Regular.ttf')
-        self.header_font_path = str(self.header_font_path_obj)
-        self.header_font_bold_path_obj = pathlib.Path('fonts/LiberationMono-Bold.ttf')
-        self.header_font_bold_path = str(self.header_font_bold_path_obj)
+        self.header_font_path = str(pathlib.Path('fonts/LiberationMono-Regular.ttf'))
+        # self.header_font_bold_path = str(pathlib.Path('fonts/LiberationMono-Bold.ttf'))
 
         # assorted colors
         self.color_white = (255, 255, 255)
@@ -44,23 +66,84 @@ class ShittySettings:
         self.color_blue = (0, 0, 255)
 
         # game state variables
+        self.shitty_mode = False
         self.exiting = False
         self.turn_black = False
 
-    def horizontal_tile_count(self) -> int:
-        """returns the amount of tiles needed to the draw board - horizontally"""
+    def pawn_path_black(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_pawn_black_path
+        return self.trad_pawn_black_path
 
-        return int(self.cols / self.cols_per_tile)
+    def pawn_path_white(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_pawn_white_path
+        return self.trad_pawn_white_path
 
-    def vertical_tile_count(self) -> int:
-        """returns the amount of tiles needed to draw the board - vertically"""
+    def rook_path_black(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_rook_black_path
+        return self.trad_rook_black_path
 
-        return int(self.rows / self.rows_per_tile)
+    def rook_path_white(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_rook_white_path
+        return self.trad_rook_white_path
+
+    def bishop_path_black(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_bishop_black_path
+        return self.trad_bishop_black_path
+
+    def bishop_path_white(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_bishop_white_path
+        return self.trad_bishop_white_path
+
+    def knight_path_black(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_knight_black_path
+        return self.trad_knight_black_path
+
+    def knight_path_white(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_knight_white_path
+        return self.trad_knight_white_path
+
+    def queen_path_black(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_queen_black_path
+        return self.trad_queen_black_path
+
+    def queen_path_white(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_queen_white_path
+        return self.trad_queen_white_path
+
+    def king_path_black(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_king_black_path
+        return self.trad_king_black_path
+
+    def king_path_white(self) -> str:
+        if self.shitty_mode:
+            return self.shitty_king_white_path
+        return self.trad_king_white_path
+
+    def space_path_black(self) -> str:
+        """returns path for current black board space image"""
+
+        return self.space_path_wood_black
+
+    def space_path_white(self) -> str:
+        """returns path for current white board image image"""
+
+        return self.space_path_wood_white
 
     def row_header_width(self) -> int:
         """returns row header width in pixels"""
 
-        return self.__row_header_w
+        return self.__row_header_width
 
     def row_header_height(self) -> int:
         """returns row header height in pixels"""
@@ -75,7 +158,7 @@ class ShittySettings:
     def col_header_height(self) -> int:
         """returns column header height in pixels"""
 
-        return self.__col_header_h
+        return self.__col_header_height
 
     def row_header_y_start(self) -> int:
         """
@@ -116,17 +199,17 @@ class ShittySettings:
     def space_width(self) -> int:
         """returns the width of a board space in pixels"""
 
-        return int(self.tile_w / self.cols_per_tile)
+        return self.__space_width
 
     def space_height(self) -> int:
         """returns the height of a board space in pixels"""
 
-        return int(self.tile_h / self.rows_per_tile)
+        return self.__space_height
 
     def board_start_x(self) -> int:
         """
         returns the x coordinate for the left of the game board
-        for use iterating the board tiles, etc
+        for use iterating the board space sprites, etc
         """
 
         if self.headers_enabled:
@@ -136,7 +219,7 @@ class ShittySettings:
     def board_start_y(self) -> int:
         """
         returns the y coordinate for the top of the game board
-        for use iterating board tiles, etc
+        for use iterating board space sprites, etc
         """
 
         if self.headers_enabled:
